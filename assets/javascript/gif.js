716eb188a7276b21e4f39d916a800de02716feb3
Ciarra Murphy <mciarra@ymail.com>
2017-09-04 19:36:34 -0400

    changed search link for gif to the static link

--- assets/javascript/gif.js
+++ assets/javascript/gif.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-
+//pulls the GIF from the web
 var key = "dc6zaTOxFJmzC";
 var getUrl = "https://api.giphy.com/v1/gifs/search?q=dog&api_key=" + key + "&limit=1";
 
@@ -8,14 +8,20 @@ $.ajax({
     method: 'GET',
 }).done(function(response){
     console.log(response);
-    var image = response.data[0].images.original.url;
+    var image = response.data[0].images.original_still.url;
     console.log(image);
-    //var imageUrl = response.data;
-    //console.log(imageUrl);
     var newImage = $('<img>');
     newImage.attr("src", image);
+    newImage.attr("data-state", 'still');
+    newImage.attr("data-animate", response.data[0].images.original.url);
     console.log(newImage);
     $("#gifs").html(newImage);
 })
+//click on the GIF to animate it
+$("img").on('click',function(){
+    
+
+});
+
 
 })
