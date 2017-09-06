c9cb73018af8466ae3b26cb8e2dc9e2478c786f9
Ciarra Murphy <mciarra@ymail.com>
2017-09-05 21:33:14 -0400

    first attempt to create buttons for array

--- assets/javascript/gif.js
+++ assets/javascript/gif.js
@@ -1,27 +1,51 @@
 $(document).ready(function(){
 //pulls the GIF from the web
-var topic = [];
+var topic = ["I Love Lucy", "Parks and Recreation", "Unbreakable Kimmy Schmidt", "The IT Crowd", "Sherlock"];
+console.log(topic);
+
 var key = "dc6zaTOxFJmzC";
-var getUrl = "https://api.giphy.com/v1/gifs/search?q=i+love+lucy&api_key=" + key + "&limit=10";
+var getUrl = "https://api.giphy.com/v1/gifs/search?q=parks+and+recreation&api_key=" + key + "&limit=10";
+
+function renderButtons(){
+    //$("#buttons").empty();
+    for(var index = 0; index < topic.length; index++){
+        var topicButton = $("<button>");
+        console.log(topicButton);
+        topicButton.addClass("show");
+        topicButton.attr("data-name", topic[index])
+        //console.log(topicButton);
+        topicButton.text(topic[index]);
+        topicButton.push("#buttons");
+        console.log("this works")
+        $("#buttons").append(topicButton);
+        $("#buttons").html(topicButton);
+    }
+
+}
 
 $.ajax({
     url: getUrl,
     method: 'GET',
 }).done(function(response){
     console.log(response);
-
-    //grabs the 10 images
-    for(var index=0; index<response.data.length; index++){
-    var image = response.data[index].images.original_still.url;
+    
+    var image = response.data[0].images.original_still.url;
     console.log(image);
+    //var newGif = $("<div class='new-gifs'>");
     var newImage = $('<img>');
     newImage.attr("src", image);
     newImage.attr("data-state", 'still');
-    newImage.attr("data-animate", response.data[index].images.original.url);
+    newImage.attr("data-animate", response.data[0].images.original_still.url);
     console.log(newImage);
-    $("#gifs").html(newImage);
-    }
+    //newGif.prepend(newImage);
+    //newGif.prepend(topic);
+    //console.log(newGif);
+    //$("#gifs").html(newImage);
+    
 })
+//displaying the 10 GIFs with a button
+
+
 //click on the GIF to animate it
 //$("img").on('click',function(){
     //let $this = $(this);
