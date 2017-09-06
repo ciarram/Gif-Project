b12ae692649614bf51cfef8d538fadf812dded1a
Ciarra Murphy <mciarra@ymail.com>
2017-09-05 23:10:47 -0400

    added rating to js file

--- assets/javascript/gif.js
+++ assets/javascript/gif.js
@@ -3,12 +3,10 @@ $(document).ready(function(){
 var topic = ["I Love Lucy", "Parks and Recreation", "Unbreakable Kimmy Schmidt", "The IT Crowd", "Sherlock"];
 console.log(topic);
 
-var key = "dc6zaTOxFJmzC";
-var getUrl = "https://api.giphy.com/v1/gifs/search?q=parks+and+recreation&api_key=" + key + "&limit=10";
 
 //function renderButtons() {
     //console.log("the function works");
-    //$("#buttons").empty();
+    $("#buttons").empty();
     for(var index = 0; index < topic.length; index++) {
         var topicButton = $("<button>").addClass("show").attr("data-name", topic[index]).text(topic[index]);
         console.log(topicButton, 'this is a button');
@@ -17,28 +15,33 @@ var getUrl = "https://api.giphy.com/v1/gifs/search?q=parks+and+recreation&api_ke
     }
 
 //}
-
-$.ajax({
+//function displayGifs(){
+var key = "dc6zaTOxFJmzC";
+var getUrl = "https://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=" + key + "&limit=10";
+    console.log("I can pick up a string");
+//$(topicButton).on('click', function(){
+    $.ajax({
     url: getUrl,
     method: 'GET',
-}).done(function(response){
-    console.log(response);
-    for(var index=0; index<response.data.length; index++){
-    var image = response.data[index].images.original_still.url;
-    console.log(image);
-    //var newGif = $("<div class='new-gifs'>");
-    var newImage = $('<img>');
-    newImage.attr("src", image);
-    newImage.attr("data-state", 'still');
-    newImage.attr("data-animate", response.data[index].images.original_still.url);
-    console.log(newImage);
-    //newGif.prepend(newImage);
-    //newGif.prepend(topic);
-    //console.log(newGif);
-    $("#gifs").append(newImage);
+    }).done(function(response){
+        console.log(response);
+        for(var i=0; i < response.data.length; i++){
+        var rating = response.data[i].rating;
+            console.log('rating',rating);
+        var printedRating = $('<p>').text("Rating: " + rating);
+            console.log('printedRating',printedRating);
+        var image = response.data[i].images.original_still.url;
+        console.log(image);
+        var newImage = $('<img>');
+        newImage.attr("src", image);
+        newImage.attr("data-state", 'still');
+        newImage.attr("data-animate", response.data[i].images.original.url);
+        console.log(newImage);
+        //$("#gifs").append(newImage);
     }   
+    //})
 })
-
+//}
 
 //displaying the 10 GIFs with a button
 
